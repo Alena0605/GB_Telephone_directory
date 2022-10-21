dad2fdb84113cc69af5155ec379e7f744f291f4d
Alena <ABelova0605@gmail.com>
2022-10-22 02:45:47 +0300

    add file user_interface.py

---
+++ user_interface.py
@@ -0,0 +1,23 @@
+def main_menu():
+    print("Choose the number: ")
+    print("1. Show all existing contacts")
+    print("2. Add new contact")
+    print("3. Delete contact")
+    print("4. Search the existing contact")
+    print("5. Import contacts")
+    print("6. Exit")
+
+
+def export_menu():
+    print("Choose the number: ")
+    print("1. Show all existing contacts")
+    print("2. Load from .csv")
+    print("3. Load from .json")
+    print("4. Return to the Main menu")
+
+
+def import_menu():
+    print("Choose the number: ")
+    print("1. Save to .csv")
+    print("2. Save to .json")
+    print("3. Return to the Main menu")
